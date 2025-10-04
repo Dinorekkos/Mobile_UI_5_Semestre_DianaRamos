bd0ee5885c94b6684bb4163b6e5990d092b81789
Diana Ramos/ Dinorekkos <diana_ramos_g@hotmail.com>
2025-10-03 20:39:23 -0600

    doshake anims

--- UI_UX_Mobile/Assets/Clase_UI_Mobile/_Code/UI/PopupUI.cs
+++ UI_UX_Mobile/Assets/Clase_UI_Mobile/_Code/UI/PopupUI.cs
@@ -1,5 +1,7 @@
 using System;
+using DG.Tweening;
 using Dino.UtilityTools.Extensions;
+using Lean.Gui;
 using TMPro;
 using UnityEngine;
 using UnityEngine.InputSystem;
@@ -8,20 +10,32 @@ using UnityEngine.UI;
 public class PopupUI : UIWindow
 {
     #region Popup implementation
-    
+
     #region serialized Fields
 
     [Header("Popup Settings")] 
     [SerializeField] private TextMeshProUGUI _text;
+
     [SerializeField] private Button _buttonYes;
     [SerializeField] private Button _buttonNo;
-    #endregion
+
+    [SerializeField] private LeanHover _yesHover;
+    [SerializeField] private LeanHover _noHover;
+    [SerializeField] private float strengthShake = 10f;
+    
+    [SerializeField] private LayoutGroup layoutGroup;
+    
+    
+#endregion
     
     public override void Initialize()
     {
         base.Initialize();
         _buttonNo.onClick.AddListener(NoClick);
         _buttonYes.onClick.AddListener(YesClick);
+        
+        _yesHover.OnEnter.AddListener(OnHoverYesButton);
+        _noHover.OnEnter.AddListener(OnHoverNoButton);
         
         _text.text = string.Empty;
     }
@@ -46,6 +60,23 @@ public class PopupUI : UIWindow
     {
         Debug.Log("No clicked");
         Hide();
+    }
+
+    private void OnHoverYesButton()
+    {
+        Debug.Log("OnHoverYesButton".SetColor(ColorDebug.Green));
+        RectTransform rectTransform = _buttonYes.GetComponent<RectTransform>();
+        rectTransform.DOAnchorPosY(rectTransform.anchoredPosition.y + 50f, 0.1f).SetEase(Ease.OutBack)
+            .OnComplete(() => LayoutRebuilder.ForceRebuildLayoutImmediate(layoutGroup.GetComponent<RectTransform>()));
+        
+    }
+    private void OnHoverNoButton()
+    {
+        Debug.Log("OnHoverNoButton".SetColor(ColorDebug.Red));
+        
+        RectTransform rectTransform = _buttonNo.GetComponent<RectTransform>();
+        rectTransform.DOShakeAnchorPos(1f,strengthShake,20,90, false).OnComplete(
+            () =>  LayoutRebuilder.ForceRebuildLayoutImmediate(layoutGroup.GetComponent<RectTransform>()));
     }
     #endregion
 }
